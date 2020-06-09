5535c2a814e8b84724569d942eacd46f23ad97cd
Nikolay Beketov <nbek@miko.ru>
2020-06-09 18:15:37 +0300

    ASK-772 Ошибка тестирования email пользователя на уникальность

--- src/Common/Models/Users.php
+++ src/Common/Models/Users.php
@@ -11,6 +11,12 @@ namespace MikoPBX\Common\Models;
 
 use Phalcon\Mvc\Model\Relation;
 
+/**
+ * Class Users
+ *
+ * @package MikoPBX\Common\Models
+ * @method static mixed findFirstByEmail(array|string|int $parameters = null)
+ */
 class Users extends ModelsBase
 {
     /**
