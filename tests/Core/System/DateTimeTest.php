f43a2f34a3e29c72ef28e6c4152c75d285860e9c
apor <apor@miko.ru>
2020-07-29 13:16:02 +0300

    TimeManagement более не существует.

--- tests/Core/System/DateTimeTest.php
+++ tests/Core/System/DateTimeTest.php
@@ -1,16 +1,14 @@
 <?php
 /**
- * Copyright (C) MIKO LLC - All Rights Reserved
+ * Copyright © MIKO LLC - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
- * Written by Nikolay Beketov, 6 2020
- *
+ * Written by Alexey Portnov, 7 2020
  */
 
 namespace MikoPBX\Tests\Core\System;
 
 use MikoPBX\Core\System\Configs\PHPConf;
-use MikoPBX\Core\System\TimeManagement;
 use MikoPBX\Core\System\MikoPBXConfig;
 use MikoPBX\Tests\Unit\AbstractUnitTest;
 
