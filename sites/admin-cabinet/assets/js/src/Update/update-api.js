21fb4310971c4779c42f7283a9338141e61f18cc
Nikolay Beketov <nbek@miko.ru>
2020-09-14 21:06:17 +0300

    Подправил ругань Scrutinizer

--- sites/admin-cabinet/assets/js/src/Update/update-api.js
+++ sites/admin-cabinet/assets/js/src/Update/update-api.js
@@ -8,11 +8,6 @@
 
 /* global Config, globalWebAdminLanguage, globalPBXLicense, globalPBXVersion */
 const UpdateApi = {
-	pbxPing: `${Config.updateUrl}/pbxcore/api/system/ping`,
-	pbxReloadAllModulesUrl: `${Config.pbxUrl}/pbxcore/api/pbx/reloadAllModules`, // Рестарт всех модулей АТС
-	pbxReloadDialplanUrl: `${Config.pbxUrl}/pbxcore/api/pbx/reloadDialplan`, // Запуск генератора dialplan, перезапуск dialplan на АТС.
-
-
 	/**
 	 * Запрашивает на сайте новые версии модулей PBX
 	 * @returns {boolean}
@@ -63,26 +58,5 @@ const UpdateApi = {
 				cbFailure(params);
 			},
 		});
-	},
-	/**
-	 * Проверка ответа на JSON
-	 * @param jsonString
-	 * @returns {boolean|any}
-	 */
-	tryParseJSON(jsonString) {
-		try {
-			const o = JSON.parse(jsonString);
-
-			// Handle non-exception-throwing cases:
-			// Neither JSON.parse(false) or JSON.parse(1234) throw errors, hence the type-checking,
-			// but... JSON.parse(null) returns null, and typeof null === "object",
-			// so we must check for that, too. Thankfully, null is falsey, so this suffices:
-			if (o && typeof o === 'object') {
-				return o;
-			}
-		} catch (e) {
-			//
-		}
-		return false;
 	},
 };
