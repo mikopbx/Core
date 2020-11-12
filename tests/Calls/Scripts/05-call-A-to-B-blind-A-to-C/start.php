6d331dd760b58bddf6301aaa8c3731a53c407245
apor <apor@miko.ru>
2020-11-12 17:09:19 +0300

    Поправил тесты. Сократил длительность.

--- tests/Calls/Scripts/05-call-A-to-B-blind-A-to-C/start.php
+++ tests/Calls/Scripts/05-call-A-to-B-blind-A-to-C/start.php
@@ -10,7 +10,7 @@ require_once __DIR__ . '/../TestCallsBase.php';
 
 $sampleCDR = [];
 $sampleCDR[] = ['src_num'=>'aNum', 'dst_num'=>'bNum', 'duration'=>'5', 'billsec'=>'4', 'fileDuration' => '3'];
-$sampleCDR[] = ['src_num'=>'bNum', 'dst_num'=>'cNum', 'duration'=>'11','billsec'=>'9', 'fileDuration' => '10'];
+$sampleCDR[] = ['src_num'=>'bNum', 'dst_num'=>'cNum', 'duration'=>'7', 'billsec'=>'5', 'fileDuration' => '5'];
 
 $testName = basename(__DIR__);
 $test = new TestCallsBase();
