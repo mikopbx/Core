da567823570965ea920994318873a7f23792eef3
apor <apor@miko.ru>
2021-04-12 17:51:21 +0300

    #77 Исправлена ошибка редиректа на https.

--- src/Core/System/Configs/NginxConf.php
+++ src/Core/System/Configs/NginxConf.php
@@ -108,8 +108,8 @@ class NginxConf extends Injectable
 
             $conf_data = 'if ( $remote_addr != "127.0.0.1" ) {' . PHP_EOL
                 . '        ' . 'return 301 https://$host:' . $WEBHTTPSPort . '$request_uri;' . PHP_EOL
-                . '       }' . PHP_EOL
-                . $includeRow. PHP_EOL;
+                . '    }' . PHP_EOL
+                . '    '.$includeRow. PHP_EOL;
             $config    = str_replace($includeRow, $conf_data, $config);
         }
         file_put_contents($httpConfigFile, $config);
