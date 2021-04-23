f7f99a19fa2f8105f589facdd22ec406e5ce8250
Weblate <noreply@weblate.org>
2021-04-23 17:23:34 +0300

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MIKOPBX/Core
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/admin-web-interface/

--- src/Common/Messages/glossary.php
+++ src/Common/Messages/glossary.php
@@ -1,12 +1,7 @@
 <?php
 return [
-    'ex_Japanese' => '日本語',
     'ex_French' => 'Le français',
-    'ex_Vietnamese' => 'Tiếng việt',
     'ex_Swedish' => 'Svenska',
-    '%message%' => '%message%',
     '%represent%' => '%represent%',
-    '%captured%' => '%captured%',
     '%free%' => '%free%',
-    '%number%' => '%number%',
 ];
