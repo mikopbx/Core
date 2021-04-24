7b114ae39b87735228d5187b2a06ed26cb52bca4
nb <nbek@miko.ru>
2021-04-24 12:50:27 +0300

    Навожу порядок с переводами

--- src/Common/Messages/zh_Hans.php
+++ src/Common/Messages/zh_Hans.php
@@ -1,22 +1,24 @@
 <?php
 return [
-    /*
-* MikoPBX - free phone system for small business
-* Copyright (C) 2017-2020 Alexey Portnov and Nikolay Beketov
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License along with this program.
-* If not, see <https://www.gnu.org/licenses/>.
-*/
+    'ex_Russian' => 'Русский',
+    'ex_English' => 'English(US)',
+    'ex_EnglishUK' => 'English(UK)',
+    'ex_Japanese' => '日本語',
+    'ex_Deutsch' => 'Deutsch',
+    'ex_Danish' => 'Dansk',
+    'ex_Spanish' => 'Espanol',
+    'ex_French' => 'Le français',
+    'ex_Italian' => 'Italiano',
+    'ex_Portuguese' => 'Português',
+    'ex_Ukrainian' => 'Український',
+    'ex_Vietnamese' => 'Tiếng việt',
+    'ex_Chinese' => '中国人',
+    'ex_Polish' => 'Polskie',
+    'ex_Dutch' => 'Nederlands',
+    'ex_Swedish' => 'Svenska',
+    'ex_Czech' => 'Čeština',
+    'ex_Turkish' => 'Türk',
+    'ex_Georgian' => 'ქართველი',
     'March' => '三月',
     'April' => '四月',
     'May' => '五月',
