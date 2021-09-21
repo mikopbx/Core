cca02f24aa7c50aadee9e4c4bd6606982c1ec30e
apor <apor@miko.ru>
2021-09-21 12:20:49 +0300

    Поправил пример lua dialplan

--- src/Core/Asterisk/Configs/ExtensionsConf.php
+++ src/Core/Asterisk/Configs/ExtensionsConf.php
@@ -82,7 +82,7 @@ class ExtensionsConf extends CoreConfigClass
 
         Util::fileWriteContent($this->config->path('asterisk.astetcdir') . '/extensions.conf', $conf);
         $confLua =  '-- extensions["test-default"] = {'.PHP_EOL.
-                    '--    ["100"] = function(context, extension);'.PHP_EOL.
+                    '--    ["100"] = function(context, extension)'.PHP_EOL.
                     '--        app.playback("please-hold");'.PHP_EOL.
                     '--    end;'.PHP_EOL.
                     '-- -- Forbidden to describe contexts defined in extensions.conf. This will cause a crash asterisk.'.PHP_EOL.
