5649ddecb4b3331fc7640ef25f9f3cb7157f9066
Weblate <noreply@weblate.org>
2021-10-16 06:33:56 +0300

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 3.8% (40 of 1040 strings)
    
    Translated using Weblate (Portuguese (Brazil))
    
    Currently translated at 3.8% (40 of 1040 strings)
    
    Co-authored-by: Anonymous <noreply@weblate.org>
    Co-authored-by: Everton Massen Goncalves <massen@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/admin-web-interface/pt_BR/
    Translation: MIKOPBX/Core

--- src/Common/Messages/pt_BR.php
+++ src/Common/Messages/pt_BR.php
@@ -1,1 +1,23 @@
 <?php
+return [
+    'Jun' => 'Mai',
+    'Apr' => 'Abr',
+    'Mar' => 'Mar',
+    'Feb' => 'Fev',
+    'Jan' => 'Jan',
+    'December' => 'Dezembro',
+    'November' => 'Novembro',
+    'October' => 'Outubro',
+    'September' => 'Setembro',
+    'August' => 'Agosto',
+    'July' => 'Julho',
+    'June' => 'Junho',
+    'May' => 'Maio',
+    'April' => 'Abril',
+    'March' => 'Março',
+    'February' => 'Fevereiro',
+    'January' => 'Janeiro',
+    'ex_EnglishUK' => 'English(UK)',
+    'ex_English' => 'English(US)',
+    'Jun' => 'Junho',
+];
