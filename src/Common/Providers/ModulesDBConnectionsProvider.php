c8be729987d2d3e9bcd231fd4382c481e58e6e8b
apor <apor@miko.ru>
2022-07-20 15:11:53 +0300

    #364

--- src/Common/Providers/ModulesDBConnectionsProvider.php
+++ src/Common/Providers/ModulesDBConnectionsProvider.php
@@ -88,7 +88,7 @@ class ModulesDBConnectionsProvider extends DatabaseProviderBase implements Servi
                 }
 
                 if (!class_exists($moduleModelClass)
-                    || 'docker-entrypoint' === cli_get_process_title()
+                    || '1' === getenv('START-DOCKER')
                     || count(get_class_vars($moduleModelClass)) === 0) {
                     continue;
                 }
