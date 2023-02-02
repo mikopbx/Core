1ee6c783a3b892fcb851809f987f21972aa80eda
apor <apor@miko.ru>
2023-02-02 18:05:06 +0300

    #361 Переработал загрузку файла с использованем библиотеки GuzzleHttp (исинхронный вариант)

--- src/PBXCoreREST/Workers/WorkerDownloader.php
+++ src/PBXCoreREST/Workers/WorkerDownloader.php
@@ -25,6 +25,7 @@ use MikoPBX\Core\Workers\WorkerBase;
 use MikoPBX\Core\System\Util;
 use GuzzleHttp;
 use GuzzleHttp\Exception\GuzzleException;
+use Psr\Http\Message\ResponseInterface;
 
 
 class WorkerDownloader extends WorkerBase
@@ -35,6 +36,8 @@ class WorkerDownloader extends WorkerBase
     private string $progress_file = '';
     private string $error_file = '';
     private int $file_size = 0;
+    private int $lastUpdate = 0;
+    private int $httpCode = 0;
 
     /**
      * WorkerDownloader entry point.
@@ -43,6 +46,7 @@ class WorkerDownloader extends WorkerBase
      */
     public function start($params): void
     {
+        $this->lastUpdate=time();
         $this->old_memory_limit = ini_get('memory_limit');
         $filename = $params[2]??'';
         if (file_exists($filename)) {
@@ -82,30 +86,45 @@ class WorkerDownloader extends WorkerBase
         }
 
         file_put_contents($this->progress_file, 0);
+        $curl = new GuzzleHttp\Handler\CurlMultiHandler();
+        $handler = GuzzleHttp\HandlerStack::create($curl);
         $client = new GuzzleHttp\Client();
-        try {
-            $res = $client->request('GET', $this->settings['url'], [
-                'sink'     => $this->settings['res_file'],
-                'progress' => [$this, 'progressGuzzle']
-            ]);
-            $http_code = $res->getStatusCode();
-        }catch ( GuzzleException  $e){
-            file_put_contents($this->error_file, $e->getMessage(), FILE_APPEND);
-            $http_code = -1;
+        $promise = $client->getAsync($this->settings['url'], [
+            'handler' => $handler,
+            'sink'     => $this->settings['res_file'],
+            'progress' =>  [$this, 'progress'],
+            'connect_timeout' => 5
+        ]);
+        $promise->then(
+            function (ResponseInterface $res) {
+                $this->httpCode = $res->getStatusCode();
+            },
+            function (GuzzleHttp\Exception\RequestException $e) {
+                file_put_contents($this->error_file, $e->getMessage(), FILE_APPEND);
+                $this->httpCode = -1;
+            }
+        );
+        while ($promise->getState() === 'pending'){
+            $curl->tick();
+            if(time() - $this->lastUpdate > 30){
+                $this->httpCode = -1;
+                file_put_contents($this->error_file, 'Fail download file... No progress for more than 30 seconds.', FILE_APPEND);
+                break;
+            }
+        }
+        if ($this->httpCode !== 200) {
+            file_put_contents($this->error_file, "Curl return code $this->httpCode. ", FILE_APPEND);
         }
 
-        if ($http_code !== 200) {
-            file_put_contents($this->error_file, "Curl return code $http_code. ", FILE_APPEND);
-        }
-
-        return $http_code === 200;
+        return $this->httpCode === 200;
     }
 
-    public function progressGuzzle( $downloadTotal, $downloadedBytes, $uploadTotal, $uploadedBytes) :void
+    public function progress( $downloadTotal, $downloadedBytes, $uploadTotal, $uploadedBytes) :void
     {
         if ($downloadedBytes === 0) {
             return;
         }
+        $this->lastUpdate = time();
         if ($this->file_size < 0) {
             $new_progress = $downloadedBytes / $downloadTotal * 100;
         } else {
@@ -113,6 +132,8 @@ class WorkerDownloader extends WorkerBase
         }
         $delta = $new_progress - $this->progress;
         if ($delta > 1) {
+            // Лимит на работу скрипта. Чтобы исключить "Зависание".
+            // Если нет прогресса, то завершать работу.
             $this->progress = round($new_progress);
             $this->progress = min($this->progress, 99);
             file_put_contents($this->progress_file, $this->progress);
@@ -131,7 +152,7 @@ class WorkerDownloader extends WorkerBase
         $fileSize = -1;
         try{
             $client    = new GuzzleHttp\Client();
-            $response  = $client->head($url);
+            $response  = $client->head($url, ['connect_timeout' => 5]);
             $fileSize  = $response->getHeader('Content-Length')[0];
         }catch ( GuzzleException  $e){
             file_put_contents($this->error_file, $e->getMessage(), FILE_APPEND);
