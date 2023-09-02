0b684f9b13137ad45f28a826dd2e5235a035e53c
Nikolay Beketov <nbek@miko.ru>
2023-09-02 15:19:47 +0700

    Rename SSHPasswordHash to SSHPasswordHashFile constant

--- src/Common/Models/PbxSettingsConstants.php
+++ src/Common/Models/PbxSettingsConstants.php
@@ -10,7 +10,7 @@ abstract class PbxSettingsConstants
 {
     const WWW_ENCRYPTION_KEY = 'WWWEncryptionKey';
     const SSH_PASSWORD = 'SSHPassword';
-    const SSH_PASSWORD_HASH_FILE = 'SSHPasswordHash';
+    const SSH_PASSWORD_HASH_FILE = 'SSHPasswordHashFile';
     const SSH_PASSWORD_HASH_STRING = 'SSHPasswordHashString';
     const SSH_DISABLE_SSH_PASSWORD = 'SSHDisablePasswordLogins';
     const SSH_AUTHORIZED_KEYS = 'SSHAuthorizedKeys';
