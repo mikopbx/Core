8649d4eb2cfa93d49d5b5b4345bd3ab6356f1fde
apor <apor@miko.ru>
2024-03-06 13:15:42 +0300

    Поправил Originate, реализовал через call файлы

--- tests/Calls/Scripts/TestCallsBase.php
+++ tests/Calls/Scripts/TestCallsBase.php
@@ -23,6 +23,7 @@ use MikoPBX\Common\Models\CallDetailRecordsTmp;
 use MikoPBX\Common\Providers\CDRDatabaseProvider;
 use MikoPBX\Core\Asterisk\AsteriskManager;
 use MikoPBX\Core\System\Processes;
+use MikoPBX\Core\System\Storage;
 use MikoPBX\Core\System\Util;
 
 require_once 'Globals.php';
@@ -184,6 +185,27 @@ class TestCallsBase {
     }
 
     /**
+     * Originate на тестовой станции.
+     * @param string $src
+     * @param string $dst
+     */
+    private function actionOriginateGeneral(string $src, string $dst){
+        self::printInfo("Start originate (general)... $src to $dst");
+        $outgoingDir = dirname(Storage::getMonitorDir()) . "/outgoing";
+        $conf = "Channel: Local/$src@internal-originate".PHP_EOL.
+            "Context: all_peers".PHP_EOL.
+            "Extension: $dst".PHP_EOL.
+            "Priority: 1".PHP_EOL.
+            "Callerid: $src".PHP_EOL.
+            "Setvar: __pt1c_cid=$dst".PHP_EOL;
+
+        $tmpFile     = tempnam('/tmp', 'call');
+        file_put_contents($tmpFile,$conf);
+        Processes::mwExec("mv $tmpFile $outgoingDir/test.call");
+        usleep(500000);
+    }
+
+    /**
      * Старт работы теста.
      * @param string $testName
      * @param array  $sampleCDR
@@ -267,9 +289,7 @@ class TestCallsBase {
         if(property_exists(self::class, $dst)){
             $dst = $this->$dst;
         }
-        self::printInfo("Start originate... $src to $dst");
-        $result = Util::amiOriginate($src, '', $dst);
-        self::printInfo('Result originate: '.$result['Response']??'none');
+        $this->actionOriginateGeneral($src, $dst);
     }
 
     private function invokeWait($rule):void{
