b46ca1fce78b451b2169b6f52c71fda45cb6d86e
Nikolay Beketov <nbek@miko.ru>
2024-03-22 09:52:31 +0800

    Fix test

--- tests/AdminCabinet/Lib/MikoPBXTestsBase.php
+++ tests/AdminCabinet/Lib/MikoPBXTestsBase.php
@@ -592,7 +592,6 @@ class MikoPBXTestsBase extends BrowserStackTest
             $xpath = "//div[contains(@class, 'menu')]//a[contains(@data-tab,'{$anchor}')]";
             $tab = self::$driver->findElement(WebDriverBy::xpath($xpath));
             $actions = new WebDriverActions(self::$driver);
-            $actions->moveToElement($tab);
             $actions->perform();
             $tab->click();
         } catch (NoSuchElementException $e) {
