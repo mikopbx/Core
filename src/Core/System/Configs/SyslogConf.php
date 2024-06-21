6f8a0b33405f2fc0bb102f51fb88d635dad0bcff
boffart <>
2024-06-21 13:38:35 +0300

    #756 Вынес лог crond в отдельный файл

--- src/Core/System/Configs/SyslogConf.php
+++ src/Core/System/Configs/SyslogConf.php
@@ -56,6 +56,10 @@ class SyslogConf extends Injectable
             }
             Processes::killByName('syslogd');
         }
+
+        RedisConf::generateSyslogConf();
+        CronConf::generateSyslogConf();
+
         Processes::safeStartDaemon(self::PROC_NAME, '-n');
     }
 
