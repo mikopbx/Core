2022529f60cd65baf35954865cf7d60c041f8e0a
Nikolay Beketov <nbek@miko.ru>
2024-10-14 15:25:48 +0700

    Update deprecation notice in PbxSettingsConstants
    Clarified the deprecation date for PbxSettingsConstants to specify it begins from 2024.2.30. This provides clearer guidance on the timeline for transitioning to PbxSettings::.

--- src/Common/Models/PbxSettingsConstants.php
+++ src/Common/Models/PbxSettingsConstants.php
@@ -3,7 +3,7 @@
 namespace MikoPBX\Common\Models;
 
 /**
- * @deprecated use PbxSettings:: instead
+ * @deprecated use PbxSettings:: instead (begins from 2024.2.30)
  * Collection of constants representing keys for PBX settings.
  * Organized into sections for easier maintenance and readability.
  */
