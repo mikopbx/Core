1537de115aaa29ae7f82f90d696551dcbea94462
Nikolay Beketov <nbek@miko.ru>
2025-04-22 10:03:00 +0700

    Add logging for Sentry configuration file deletion in SentryConf class
    - Introduced an info log statement to indicate when the Sentry configuration file is deleted, enhancing traceability of configuration changes.

--- src/Core/System/Configs/SentryConf.php
+++ src/Core/System/Configs/SentryConf.php
@@ -64,6 +64,7 @@ class SentryConf extends Injectable
             file_put_contents(self::CONF_FILE, $conf);
 
         } elseif (file_exists(self::CONF_FILE)) {
+            $this->logger->writeInfo('Sentry conf file exists, deleting it ' . self::CONF_FILE);
             unlink(self::CONF_FILE);
         }
     }
