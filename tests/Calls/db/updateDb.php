9dc7254ced38e393c2e3bc18f4b9c9c601cce4d0
Nikolay Beketov <nbek@miko.ru>
2025-09-19 18:09:48 +0700

    refactor: update Asterisk configuration reload methods in updateDb.php
    - Replaced PBX reload calls with static reload methods from respective Asterisk configuration classes.
    - Enhanced maintainability and consistency in the database update process.

--- tests/Calls/db/updateDb.php
+++ tests/Calls/db/updateDb.php
@@ -1,19 +1,27 @@
 #!/usr/bin/php
 <?php
 require_once 'Globals.php';
+use MikoPBX\Core\Asterisk\Configs\AsteriskConf;
+use MikoPBX\Core\Asterisk\Configs\ExtensionsConf;
+use MikoPBX\Core\Asterisk\Configs\FeaturesConf;
+use MikoPBX\Core\Asterisk\Configs\IndicationConf;
+use MikoPBX\Core\Asterisk\Configs\ManagerConf;
+use MikoPBX\Core\Asterisk\Configs\ModulesConf;
+use MikoPBX\Core\Asterisk\Configs\SIPConf;
+use MikoPBX\Core\Asterisk\Configs\VoiceMailConf;
 use MikoPBX\Core\System\Upgrade\UpdateDatabase;
 use MikoPBX\Core\System\Util;
-use MikoPBX\Core\System\PBX;
 
 $dbUpdater = new UpdateDatabase();
 $dbUpdater->updateDatabaseStructure();
-PBX::sipReload();
-PBX::dialplanReload();
-PBX::managerReload();
-PBX::modulesReload();
-PBX::featuresReload();
-PBX::voicemailReload();
-PBX::coreReload();
+SIPConf::reload();;
+ExtensionsConf::reload();
+ManagerConf::reload();
+ModulesConf::reload();
+FeaturesConf::reload();
+VoiceMailConf::reload();
+AsteriskConf::reload();
+IndicationConf::reload();
 
 $cmd = Util::which('pbx-console');
 shell_exec("$cmd services restart-all");
