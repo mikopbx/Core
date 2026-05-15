mikopbx/Core

commit c0a5527e53272a521724d0b1c8ed7744ffba35f1

Translate comments into English rewrite phpdoc blocks #522

src/Core/Workers/Libs/WorkerCallEvents/ActionTransferDialHangup.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * MikoPBX - free phone system for small business
- * Copyright © 2017-2021 Alexey Portnov and Nikolay Beketov
+ * Copyright © 2017-2023 Alexey Portnov and Nikolay Beketov
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,14 +24,22 @@ use MikoPBX\Common\Models\CallDetailRecordsTmp;
 use MikoPBX\Core\System\Util;
 use MikoPBX\Core\Workers\WorkerCallEvents;
 
+/**
+ * Class ActionTransferDialHangup
+ *
+ *
+ *  @package MikoPBX\Core\Workers\Libs\WorkerCallEvents
+ */
 class ActionTransferDialHangup
 {
     /**
-     * Завершение канала при прееадресации.
-     * @param $worker
-     * @param $data
+     * Executes the transfer dial hangup action.
+     *
+     * @param WorkerCallEvents $worker The worker instance.
+     * @param array $data The event data.
+     * @return void
      */
-    public static function execute(WorkerCallEvents $worker, $data):void
+    public static function execute(WorkerCallEvents $worker, $data): void
     {
         $pos = stripos($data['agi_channel'], 'local/');
         if ($pos === false) {
@@ -43,11 +51,74 @@ class ActionTransferDialHangup
     }
 
     /**
-     * Заполнение не отвеченного звонка.
-     * @param WorkerCallEvents $worker
-     * @param $data
+     * Fills the local channel CDR.
+     *
+     * @param WorkerCallEvents $worker The worker object.
+     * @param array $data The data array.
+     *
+     * @return void
      */
-    private static function fillNotAnsweredCdr($worker, $data):void{
+    private static function fillLocalChannelCdr(WorkerCallEvents $worker, $data): void
+    {
+
+        // This is NOT a local channel.
+        // If it's a transfer completion (advisory), create a new CDR row.
+        CreateRowTransfer::execute($worker, 'transfer_dial_hangup', $data);
+
+        // Find previously recorded rows.
+        $filter = [
+            'linkedid=:linkedid: AND endtime = "" AND (src_chan=:chan: OR dst_chan=:chan:)',
+            'bind' => [
+                'linkedid' => $data['linkedid'],
+                'chan' => $data['agi_channel'],
+            ],
+        ];
+        /** @var CallDetailRecordsTmp $m_data */
+        /** @var CallDetailRecordsTmp $row */
+        $m_data = CallDetailRecordsTmp::find($filter);
+        foreach ($m_data as $row) {
+            // Complete the call in CDR.
+            $row->writeAttribute('endtime', $data['end']);
+            $row->writeAttribute('transfer', 0);
+            if (!$row->save()) {
+                Util::sysLogMsg('Action_transfer_dial_answer', implode(' ', $row->getMessages()), LOG_DEBUG);
+            }
+        }
+
+        // Try to resume conversation recording.
+        $filter = [
+            'linkedid=:linkedid: AND endtime = "" AND transfer=1',
+            'bind' => [
+                'linkedid' => $data['linkedid'],
+            ],
+        ];
+        /** @var CallDetailRecordsTmp $res */
+        $res = CallDetailRecordsTmp::findFirst($filter);
+        if ($res !== null) {
+            $info = pathinfo((string)$res->recordingfile);
+            $data_time = (empty($res->answer)) ? $res->start : $res->answer;
+            if ($data_time === null) {
+                $data_time = 'now';
+            }
+            $subDir = date('Y/m/d/H/', strtotime($data_time));
+
+            // Resume recording if monitoring is enabled.
+            if ($worker->enableMonitor($res->src_num, $res->dst_num)) {
+                $worker->MixMonitor($res->dst_chan, $info['filename'], $subDir, null, 'fillLocalChannelCdr');
+            }
+        }
+    }
+
+    /**
+     * Fills the not answered CDR.
+     *
+     * @param WorkerCallEvents $worker The worker object.
+     * @param array $data The data array.
+     *
+     * @return void
+     */
+    private static function fillNotAnsweredCdr(WorkerCallEvents $worker, $data): void
+    {
         $filter = [
             'linkedid=:linkedid: AND endtime = "" AND (src_chan=:src_chan: AND dst_chan=:dst_chan:)',
             'bind' => [
@@ -60,10 +131,10 @@ class ActionTransferDialHangup
         /** @var CallDetailRecordsTmp $row */
         $m_data = CallDetailRecordsTmp::find($filter);
         foreach ($m_data as $row) {
-            // Ответа не было. Переадресация отменена.
+            // There was no answer. The transfer was canceled.
             $row->writeAttribute('endtime', $data['end']);
             $row->writeAttribute('transfer', 0);
-            if ( ! $row->save()) {
+            if (!$row->save()) {
                 Util::sysLogMsg('Action_transfer_dial_answer', implode(' ', $row->getMessages()), LOG_DEBUG);
             }
         }
@@ -74,74 +145,26 @@ class ActionTransferDialHangup
             ],
         ];
         $m_data = CallDetailRecordsTmp::find($filter);
-        if($m_data->count() !== '1'){
-            // Переадресация не завершена или больше не существуют каналы.
+        if ($m_data->count() !== '1') {
+            // The transfer is not completed or channels no longer exist.
             return;
         }
-        // Попробуем возобновить запись разговора.
+
+        // Try to resume conversation recording.
         foreach ($m_data as $row) {
-            $info      = pathinfo($row->recordingfile);
+            $info = pathinfo($row->recordingfile);
             $data_time = ($row->answer === '') ? $row->start : $row->answer;
-            $subDir    = date('Y/m/d/H/', strtotime($data_time));
-            if($worker->enableMonitor($row->src_num, $row->dst_num)) {
+            $subDir = date('Y/m/d/H/', strtotime($data_time));
+
+            // Resume recording if monitoring is enabled.
+            if ($worker->enableMonitor($row->src_num, $row->dst_num)) {
                 $worker->MixMonitor($row->dst_chan, $info['filename'], $subDir, null, 'fillNotAnsweredCdr');
             }
-            // Снимем со строк признак переадресации.
+
+            // Remove the transfer flag from the rows.
             $row->writeAttribute('transfer', 0);
-            if ( ! $row->save()) {
+            if (!$row->save()) {
                 Util::sysLogMsg('Action_transfer_dial_answer', implode(' ', $row->getMessages()), LOG_DEBUG);
-            }
-        }
-    }
-
-    /**
-     * Обработка Local канала.
-     * @param WorkerCallEvents $worker
-     * @param $data
-     */
-    private static function fillLocalChannelCdr($worker, $data):void{
-
-        // Это НЕ локальный канал.
-        // Если это завершение переадресации (консультативной). Создадим новую строку CDR.
-        CreateRowTransfer::execute($worker, 'transfer_dial_hangup', $data);
-
-        // Найдем записанные ранее строки.
-        $filter = [
-            'linkedid=:linkedid: AND endtime = "" AND (src_chan=:chan: OR dst_chan=:chan:)',
-            'bind' => [
-                'linkedid' => $data['linkedid'],
-                'chan'     => $data['agi_channel'],
-            ],
-        ];
-        /** @var CallDetailRecordsTmp $m_data */
-        /** @var CallDetailRecordsTmp $row */
-        $m_data = CallDetailRecordsTmp::find($filter);
-        foreach ($m_data as $row) {
-            // Завершим вызов в CDR.
-            $row->writeAttribute('endtime', $data['end']);
-            $row->writeAttribute('transfer', 0);
-            if ( ! $row->save()) {
-                Util::sysLogMsg('Action_transfer_dial_answer', implode(' ', $row->getMessages()), LOG_DEBUG);
-            }
-        }
-        // Попробуем возобновить запись разговора.
-        $filter = [
-            'linkedid=:linkedid: AND endtime = "" AND transfer=1',
-            'bind' => [
-                'linkedid' => $data['linkedid'],
-            ],
-        ];
-        /** @var CallDetailRecordsTmp $res */
-        $res = CallDetailRecordsTmp::findFirst($filter);
-        if ($res !== null) {
-            $info      = pathinfo((string)$res->recordingfile);
-            $data_time = (empty($res->answer)) ? $res->start : $res->answer;
-            if ($data_time===null){
-                $data_time = 'now';
-            }
-            $subDir    = date('Y/m/d/H/', strtotime($data_time));
-            if($worker->enableMonitor($res->src_num, $res->dst_num)) {
-                $worker->MixMonitor($res->dst_chan, $info['filename'], $subDir, null, 'fillLocalChannelCdr');
             }
         }
     }

src/Core/Rc/emailTestConnection.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * MikoPBX - free phone system for small business
- * Copyright © 2017-2021 Alexey Portnov and Nikolay Beketov
+ * Copyright © 2017-2023 Alexey Portnov and Nikolay Beketov
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

src/Core/System/CloudProvisioning.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * MikoPBX - free phone system for small business
- * Copyright © 2017-2021 Alexey Portnov and Nikolay Beketov
+ * Copyright © 2017-2023 Alexey Portnov and Nikolay Beketov
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

src/Core/System/Configs/VmToolsConf.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * MikoPBX - free phone system for small business
- * Copyright © 2017-2021 Alexey Portnov and Nikolay Beketov
+ * Copyright © 2017-2023 Alexey Portnov and Nikolay Beketov
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

src/Core/Workers/Cron/WorkerSafeScriptsCore.php
@@ -59,7 +59,7 @@ class WorkerSafeScriptsCore extends WorkerBase
     /**
      * Restarts all registered workers.
      *
-     * @throws \Throwable
+     * @throws Throwable
      */
     public function restart(): void
     {
@@ -89,11 +89,11 @@ class WorkerSafeScriptsCore extends WorkerBase
     {
         // Initialize the workers' list.
         // Each worker type corresponds to a list of workers.
-        $arrWorkers        = [
-            self::CHECK_BY_AMI           =>
+        $arrWorkers = [
+            self::CHECK_BY_AMI =>
                 [
                 ],
-            self::CHECK_BY_BEANSTALK     =>
+            self::CHECK_BY_BEANSTALK =>
                 [
                     WorkerApiCommands::class,
                     WorkerCdr::class,
@@ -129,25 +129,11 @@ class WorkerSafeScriptsCore extends WorkerBase
     }
 
     /**
-     * Restarts a worker by class name.
-     *
-     * @param string $workerClassName The class name of the worker.
-     *
-     * @return \Generator|null
-     */
-    public function restartWorker(string $workerClassName): ?Generator
-    {
-        // Restart the worker and yield control back to the calling code.
-        Processes::processPHPWorker($workerClassName, 'start','restart');
-        yield;
-    }
-
-    /**
      * Starts all workers or checks them.
      *
      * @param array $params The command-line arguments.
      *
-     * @throws \Throwable
+     * @throws Throwable
      */
     public function start(array $params): void
     {
@@ -185,16 +171,16 @@ class WorkerSafeScriptsCore extends WorkerBase
      *
      * @param string $workerClassName The class name of the worker.
      *
-     * @return \Generator|null
+     * @return Generator|null
      */
     public function checkWorkerBeanstalk(string $workerClassName): ?Generator
     {
         // Check if the worker is alive. If not, restart it.
         // The check is done by pinging the worker using a Beanstalk queue.
         try {
-            $start     = microtime(true);
+            $start = microtime(true);
             $WorkerPID = Processes::getPidOfProcess($workerClassName);
-            $result    = false;
+            $result = false;
             if ($WorkerPID !== '') {
                 // We had service PID, so we will ping it
                 $queue = new BeanstalkClient($this->makePingTubeName($workerClassName));
@@ -205,7 +191,7 @@ class WorkerSafeScriptsCore extends WorkerBase
                 Processes::processPHPWorker($workerClassName);
                 Util::sysLogMsg(__METHOD__, "Service {$workerClassName} started.", LOG_NOTICE);
             }
-            $timeElapsedSecs = round(microtime(true) - $start,2);
+            $timeElapsedSecs = round(microtime(true) - $start, 2);
             if ($timeElapsedSecs > 10) {
                 Util::sysLogMsg(
                     __METHOD__,
@@ -226,18 +212,18 @@ class WorkerSafeScriptsCore extends WorkerBase
      *
      * @param string $workerClassName The class name of the worker.
      *
-     * @return \Generator|null
+     * @return Generator|null
      */
     public function checkPidNotAlert(string $workerClassName): Generator
     {
         // Check if the worker is alive based on its PID. If not, restart it.
-        $start     = microtime(true);
+        $start = microtime(true);
         $WorkerPID = Processes::getPidOfProcess($workerClassName);
-        $result    = ($WorkerPID !== '');
+        $result = ($WorkerPID !== '');
         if (false === $result) {
             Processes::processPHPWorker($workerClassName);
         }
-        $timeElapsedSecs = round(microtime(true) - $start,2);
+        $timeElapsedSecs = round(microtime(true) - $start, 2);
         if ($timeElapsedSecs > 10) {
             Util::sysLogMsg(
                 __CLASS__,
@@ -255,19 +241,19 @@ class WorkerSafeScriptsCore extends WorkerBase
      * @param string $workerClassName The class name of the worker.
      * @param int $level The recursion level.
      *
-     * @return \Generator|null
+     * @return Generator|null
      */
     public function checkWorkerAMI(string $workerClassName, int $level = 0): ?Generator
     {
         // Check if the worker is alive. If not, restart it.
         // The check is done by pinging the worker using an AMI UserEvent.
         try {
-            $start     = microtime(true);
-            $res_ping  = false;
+            $start = microtime(true);
+            $res_ping = false;
             $WorkerPID = Processes::getPidOfProcess($workerClassName);
             if ($WorkerPID !== '') {
                 // We have the service PID, so we will ping it
-                $am       = Util::getAstManager();
+                $am = Util::getAstManager();
                 $res_ping = $am->pingAMIListener($this->makePingTubeName($workerClassName));
                 if (false === $res_ping) {
                     Util::sysLogMsg(__METHOD__, 'Restart...', LOG_ERR);
@@ -283,7 +269,7 @@ class WorkerSafeScriptsCore extends WorkerBase
                 // Check service again
                 $this->checkWorkerAMI($workerClassName, $level + 1);
             }
-            $timeElapsedSecs = round(microtime(true) - $start,2);
+            $timeElapsedSecs = round(microtime(true) - $start, 2);
             if ($timeElapsedSecs > 10) {
                 Util::sysLogMsg(
                     __METHOD__,
@@ -298,6 +284,20 @@ class WorkerSafeScriptsCore extends WorkerBase
         }
         yield;
     }
+
+    /**
+     * Restarts a worker by class name.
+     *
+     * @param string $workerClassName The class name of the worker.
+     *
+     * @return Generator|null
+     */
+    public function restartWorker(string $workerClassName): ?Generator
+    {
+        // Restart the worker and yield control back to the calling code.
+        Processes::processPHPWorker($workerClassName, 'start', 'restart');
+        yield;
+    }
 }
 
 // Start worker process
@@ -308,7 +308,7 @@ try {
     if (isset($argv) && count($argv) > 1) {
         cli_set_process_title("{$workerClassname} {$argv[1]}");
         $activeProcesses = Processes::getPidOfProcess("{$workerClassname} {$argv[1]}", posix_getpid());
-        if (!empty($activeProcesses)){
+        if (!empty($activeProcesses)) {
             Util::sysLogMsg($workerClassname, "WARNING: Other started process {$activeProcesses} with parameter: {$argv[1]} is working now...", LOG_DEBUG);
             return;
         }

commit 88c0574dce38c37fdd2d9a2eb864d50f1c711dac

fix(cdr,asterisk): resume MixMonitor after failed attended transfer
Two regressions made the call recording stop at the moment of an
attended-transfer attempt and never resume, even when the original
A↔B bridge survives — losing ~50% of the conversation audio.

Lua dialplan: skip_special_exten() guard was added around
transfer_dial_hangup in ec173025e ("fix(upload,cdr,security): chunk
size, CEL guard, rate limit whitelist"). That guard short-circuits
EXTEN='h', which is exactly the path Asterisk uses for the hangup
handler — so event_transfer_dial_hangup() never fired, and the AMI
UserEvent never reached WorkerCallEvents. Removed the guard
(hangup_chan and hangup_chan_meetme already correctly run guard-free).

PHP recovery action: ActionTransferDialHangup::fillNotAnsweredCdr was
silently broken in c0a5527e53 ("Translate comments into English rewrite
phpdoc blocks #522") — that commit replaced OR with AND in the CDR
filter and substituted agi_channel for the empty dst_chan, so the
transfer-attempt row was never closed; it also added an int-vs-string
comparison count() !== '1' that always returned early. Filter now
matches on transfer="1" AND src_chan=TRANSFERERNAME, and the count
check is count() !== 1. The success path (fillLocalChannelCdr) is
untouched.

CheckSSHConfig: /etc/shadow integrity check now skips when
SSHDisablePasswordLogins=true. PAM bookkeeping (lastchg, passwd -l,
etc.) mutates shadow even when password-based SSH login is disabled,
producing spurious "SSH password changed externally" alerts.

tests/Calls infrastructure also gets several fixes used to surface
and reproduce the bug:

  * start.sh::cleanup() no longer runs 'monit stop all' (that locks
    out the host via dropbear). It stops only asterisk + php-fpm,
    waits for DB handles to release, removes stale -wal/-shm before
    restoring the production DB, then brings services back. Without
    the -wal/-shm cleanup the cp leaves the DB in "malformed" state.

src/Core/Asterisk/Configs/lua/extensions.lua
@@ -1947,7 +1947,13 @@ extensions.lua_dial_create_chan["_.!"]      = function() if not skip_special_ext
 extensions.dial_answer["_.!"]               = function() if not skip_special_exten() then event_dial_answer() end end
 extensions.lua_transfer_dial_create_chan["_.!"] = function() if not skip_special_exten() then event_transfer_dial_create_chan() end end
 extensions.transfer_dial_answer["_.!"]      = function() if not skip_special_exten() then event_transfer_dial_answer() end end
-extensions.transfer_dial_hangup["_.!"]      = function() if not skip_special_exten() then event_transfer_dial_hangup() end end
+-- transfer_dial_hangup runs from two paths:
+--   1) `exten => h,1,Goto(transfer_dial_hangup,h,1)` (hangup handler, EXTEN='h')
+--   2) `Gosub(transfer_dial_hangup,${EXTEN},1)` (post-Dial, EXTEN=dialed number)
+-- event_transfer_dial_hangup() branches on EXTEN internally and is safe in both cases —
+-- never gate it with skip_special_exten(): that suppressed the 'h' path entirely and
+-- broke MixMonitor resume after failed attended transfers.
+extensions.transfer_dial_hangup["_.!"]      = function() event_transfer_dial_hangup() end
 extensions.hangup_chan["_.!"]               = function() event_hangup_chan() end
 extensions.queue_start["_.!"]               = function() if not skip_special_exten() then event_queue_start() end end
 extensions.queue_answer["_.!"]              = function() if not skip_special_exten() then event_queue_answer() end end

src/Core/Workers/Libs/WorkerCallEvents/ActionTransferDialHangup.php
@@ -126,12 +126,14 @@ class ActionTransferDialHangup
      */
     private static function fillNotAnsweredCdr(WorkerCallEvents $worker, array $data): void
     {
+        // Close the transfer-attempt CDR. When the transfer target never picked up
+        // (e.g. CHANUNAVAIL / NO_CONTACTS), the row has transfer=1 and an empty dst_chan —
+        // matching on src_chan=TRANSFERERNAME is enough to identify it.
         $filter = [
-            'linkedid=:linkedid: AND endtime = "" AND (src_chan=:src_chan: AND dst_chan=:dst_chan:)',
+            'linkedid=:linkedid: AND endtime = "" AND transfer = "1" AND src_chan = :src_chan:',
             'bind' => [
                 'linkedid' => $data['linkedid'],
                 'src_chan' => $data['TRANSFERERNAME'],
-                'dst_chan' => empty($data['dst_chan'])?$data['agi_channel']:$data['dst_chan'],
             ],
         ];
         /** @var CallDetailRecordsTmp $m_data */
@@ -152,7 +154,8 @@ class ActionTransferDialHangup
             ],
         ];
         $m_data = CallDetailRecordsTmp::find($filter);
-        if ($m_data->count() !== '1') {
+        // Phalcon Resultset::count() returns int — compare as int, not against string '1'.
+        if ($m_data->count() !== 1) {
             // The transfer is not completed or channels no longer exist.
             return;
         }

src/Core/Workers/Libs/WorkerPrepareAdvice/CheckSSHConfig.php
@@ -42,11 +42,25 @@ class CheckSSHConfig extends Injectable
      * The SSH password is now stored as SHA-512 hash in SSH_PASSWORD,
      * so we only check if /etc/shadow was modified outside of MikoPBX.
      *
+     * When password-based SSH logins are disabled (SSHDisablePasswordLogins=true),
+     * /etc/shadow is not security-relevant for SSH access — dropbear accepts
+     * public-key auth only. PAM bookkeeping (lastchg, locking, system maintenance)
+     * still mutates /etc/shadow and would otherwise raise spurious alerts.
+     *
      * @return array<string, array<int, array<string, mixed>>> An array containing warning messages.
      */
     public function process(): array
     {
         $messages = [];
+
+        $passwordLoginsDisabled = PbxSettings::getValueByKey(
+            PbxSettings::SSH_DISABLE_SSH_PASSWORD,
+            false
+        );
+        if (filter_var($passwordLoginsDisabled, FILTER_VALIDATE_BOOLEAN)) {
+            return $messages;
+        }
+
         $hashFile = PbxSettings::getValueByKey(PbxSettings::SSH_PASSWORD_HASH_FILE, false);
 
         // Check if /etc/shadow was modified outside of MikoPBX

tests/Calls/Scripts/09-call-A-to-B-attended-B-to-offNum/start.php
@@ -20,9 +20,11 @@ use MikoPBX\Tests\Calls\Scripts\TestCallsBase;
 require_once __DIR__.'/../TestCallsBase.php';
 
 $sampleCDR = [];
+// Main A↔B conversation continues after the failed attended-transfer attempt;
+// the call recording resumes within the same CDR (single recording file).
 $sampleCDR[] = ['src_num'=>'aNum', 'dst_num'=>'bNum',  'duration'=>'14', 'billsec'=>'12', 'fileDuration' => '7'];
-$sampleCDR[] = ['src_num'=>'bNum', 'dst_num'=>'offNum','duration'=>'1',  'billsec'=>'0',  'fileDuration' => '0'];
-$sampleCDR[] = ['src_num'=>'bNum', 'duration'=>'3',  'billsec'=>'2',  'fileDuration' => '2'];
-$sampleCDR[] = ['src_num'=>'aNum', 'duration'=>'5',  'billsec'=>'5',  'fileDuration' => '5'];
+// Attended-transfer attempt to an unreachable offNum — separate CDR, NO_ANSWER, no recording.
+// Asterisk retries the unreachable PJSIP endpoint for ~6s before giving up.
+$sampleCDR[] = ['src_num'=>'bNum', 'dst_num'=>'offNum','duration'=>'6',  'billsec'=>'0',  'fileDuration' => '0'];
 
 TestCallsBase::executeTest($sampleCDR);

tests/Calls/Scripts/TestCallsBase.php
@@ -20,6 +20,7 @@
 namespace MikoPBX\Tests\Calls\Scripts;
 use MikoPBX\Common\Models\CallDetailRecords;
 use MikoPBX\Common\Models\CallDetailRecordsTmp;
+use MikoPBX\Common\Models\ExtensionForwardingRights;
 use MikoPBX\Common\Providers\CDRDatabaseProvider;
 use MikoPBX\Core\Asterisk\AsteriskManager;
 use MikoPBX\Core\System\Directories;
@@ -89,16 +90,33 @@ class TestCallsBase {
         return $db_data;
     }
 
-    /** Возвращает недоступные пиры */
+    /**
+     * Returns unreachable peers (PJSIP state != OK).
+     * Skips peers that have any forwarding configured (forwarding / forwardingonbusy /
+     * forwardingonunavailable). Otherwise test 09 receives an ANSWERED CDR to the
+     * redirect target instead of the expected NO_ANSWER on the offline endpoint.
+     */
     public static function getOffPeers():array{
         $am = Util::getAstManager('off');
         $result = $am->getPjSipPeers();
-        $db_data = array();
-        foreach ($result as $peer){
-            if($peer['state']!== 'OK' && is_numeric($peer['id'])){
-                $db_data[] = $peer['id'];
+        $forwardedExtensions = [];
+        $fwdRows = ExtensionForwardingRights::find();
+        foreach ($fwdRows as $row){
+            if (!empty($row->forwarding) || !empty($row->forwardingonbusy) || !empty($row->forwardingonunavailable)){
+                $forwardedExtensions[$row->extension] = true;
             }
         }
+        $db_data = array();
+        foreach ($result as $peer){
+            if($peer['state'] === 'OK' || !is_numeric($peer['id'])){
+                continue;
+            }
+            if(isset($forwardedExtensions[$peer['id']])){
+                continue;
+            }
+            $db_data[] = $peer['id'];
+        }
+        sort($db_data, SORT_NUMERIC);
         return $db_data;
     }
 

tests/Calls/db/updateDb.php
@@ -1,6 +1,7 @@
 #!/usr/bin/php
 <?php
 require_once 'Globals.php';
+use MikoPBX\Common\Models\ExtensionForwardingRights;
 use MikoPBX\Core\Asterisk\Configs\AsteriskConf;
 use MikoPBX\Core\Asterisk\Configs\ExtensionsConf;
 use MikoPBX\Core\Asterisk\Configs\FeaturesConf;
@@ -14,6 +15,18 @@ use MikoPBX\Core\System\Util;
 
 $dbUpdater = new UpdateDatabase();
 $dbUpdater->updateDatabaseStructure();
+
+// Test 09 (call-A-to-B-attended-B-to-offNum) needs at least one offline SIP peer
+// with NO forwarding configured: otherwise the attended-transfer attempt is redirected
+// to the mobile number and the test gets ANSWERED CDR via trunk instead of NO_ANSWER.
+// Keep extension 504 forwarding-free regardless of how the test DB was generated.
+foreach (ExtensionForwardingRights::find(['extension = :ext:', 'bind' => ['ext' => '504']]) as $row) {
+    $row->writeAttribute('forwarding', '');
+    $row->writeAttribute('forwardingonbusy', '');
+    $row->writeAttribute('forwardingonunavailable', '');
+    $row->save();
+}
+
 SIPConf::reload();;
 ExtensionsConf::reload();
 ManagerConf::reload();

tests/Calls/start.sh
@@ -38,8 +38,38 @@ function cleanup() {
 
     # Восстанавливаем production базу данных
     if [ "$didSwapDb" = "true" ] && [ -f "$dumpConfFile" ]; then
+        echo_info "Stopping services holding the DB...";
+        # Stop only the services that hold mikopbx.db open. NEVER call `monit stop all`:
+        # it brings down dropbear/sshd and locks the host out of remote access.
+        monit stop asterisk > /dev/null 2>&1 || true;
+        monit stop php-fpm  > /dev/null 2>&1 || true;
+
+        # Wait up to ~30s for processes to release the DB.
+        local i;
+        for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do
+            if [ -z "$(lsof "$confFile" 2>/dev/null | awk 'NR>1')" ]; then
+                break;
+            fi
+            sleep 2;
+        done
+
+        # Force-kill any stragglers (PHP workers outside monit). Never touches the SSH session.
+        local stragglers;
+        stragglers=$(lsof "$confFile" 2>/dev/null | awk 'NR>1 {print $2}' | sort -u);
+        if [ -n "$stragglers" ]; then
+            echo "$stragglers" | xargs -r kill -TERM 2>/dev/null || true;
+            sleep 2;
+            stragglers=$(lsof "$confFile" 2>/dev/null | awk 'NR>1 {print $2}' | sort -u);
+            [ -n "$stragglers" ] && echo "$stragglers" | xargs -r kill -KILL 2>/dev/null || true;
+        fi
+
         echo_info "Restoring production database...";
+        # Remove stale WAL/SHM from the test DB BEFORE cp.
+        # Otherwise SQLite will replay the old WAL on top of the restored file and the DB
+        # ends up "malformed" — exactly the corruption we observed before this fix.
+        rm -f "${confFile}-wal" "${confFile}-shm";
         cp "$dumpConfFile" "$confFile";
+        sync;
 
         if [ "${debugParams}x" != "x" ]; then
             export XDEBUG_CONFIG="${EMPTY}";
@@ -47,6 +77,10 @@ function cleanup() {
         php -f "$dirName/db/updateDb.php" > /dev/null 2>/dev/null;
         iptables -F 2>/dev/null; iptables -X 2>/dev/null;
         export XDEBUG_CONFIG="${debugParams}";
+
+        # Bring services back via monit, named (not `start all`) — see comment above.
+        monit start php-fpm  > /dev/null 2>&1 || true;
+        monit start asterisk > /dev/null 2>&1 || true;
     fi
 }
 trap cleanup EXIT INT TERM